9f678de2e9ebc4e25b45bf4426b7be316a42cb19
tzihklearn <1922941549@qq.com>
2023-10-02 20:53:10 +0800

    feat(tcl):搭建初步geo接口的架构

---
+++ tcl-server/src/main/java/org/sipc/tclserver/controller/GeoController.java
@@ -0,0 +1,15 @@
+package org.sipc.tclserver.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author tzih
+ * @version v1.0
+ * @since 2023.10.02
+ */
+@RestController
+public class GeoController {
+
+
+
+}
---
+++ tcl-server/src/main/java/org/sipc/tclserver/service/impl/GeoServiceImpl.java
@@ -0,0 +1,13 @@
+package org.sipc.tclserver.service.impl;
+
+import org.sipc.tclserver.service.GeoService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author tzih
+ * @version v1.0
+ * @since 2023.10.02
+ */
+@Service
+public class GeoServiceImpl implements GeoService {
+}
