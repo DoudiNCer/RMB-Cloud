2fd2a20ed23f49bf6a707bf4eafda93574d2ecab
tzihklearn <1922941549@qq.com>
2023-10-02 18:48:26 +0800

    int(tcl):初始化tcl子模块

---
+++ tcl-server/src/main/java/org/sipc/tclserver/TclServerApplication.java
@@ -0,0 +1,11 @@
+package org.sipc.tclserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TclServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TclServerApplication.class, args);
+    }
+}
