bc6766233381cc79331d2965150950b91968a2ce
tzihklearn <1922941549@qq.com>
2023-10-02 21:39:09 +0800

    feat(tcl):搭建garbage相关接口骨架

---
+++ tcl-server/src/main/java/org/sipc/tclserver/service/impl/GarbageServiceImpl.java
@@ -0,0 +1,16 @@
+package org.sipc.tclserver.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.sipc.tclserver.service.GarbageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author tzih
+ * @version v1.0
+ * @since 2023.10.02
+ */
+@Service
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+public class GarbageServiceImpl implements GarbageService {
+}
