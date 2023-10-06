cdf3c4eb809bb984054c00fb5bdd575c942be426
DoudiNCer <doudi@outlook.lv>
2023-10-06 13:55:42 +0800

    feat(*): User Server 实现 B/C端用户注册/登录/鉴权接口

---
+++ user-server/src/main/java/org/sipc/userserver/controller/UserBController.java
@@ -0,0 +1,37 @@
+package org.sipc.userserver.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.sipc.userserver.pojo.domain.UserC;
+import org.sipc.userserver.pojo.dto.CommonResult;
+import org.sipc.userserver.pojo.dto.param.userC.CTokenVerifyParam;
+import org.sipc.userserver.pojo.dto.param.userC.UserCLoginParam;
+import org.sipc.userserver.pojo.dto.param.userC.UserCRegistParam;
+import org.sipc.userserver.pojo.dto.result.userC.UserCLoginResult;
+import org.sipc.userserver.service.UserCService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user/b")
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+public class UserBController {
+
+    private final UserCService userCService;
+
+    @PostMapping("/login")
+    public CommonResult<UserCLoginResult> userBLogin(@Validated @RequestBody UserCLoginParam param){
+        return userCService.login(param);
+    }
+
+    @PostMapping("/regist")
+    public CommonResult<String> userBRRegister(@Validated @RequestParam UserCRegistParam param){
+        return userCService.register(param);
+    }
+
+    @PostMapping("/varify")
+    public CommonResult<UserC> verifyBToken(@Validated @RequestParam CTokenVerifyParam param){
+        return userCService.verifyToken(param);
+    }
+
+}
