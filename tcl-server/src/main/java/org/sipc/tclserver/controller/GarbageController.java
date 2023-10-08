2e0aa19653fb9110d73d9b4581309eef2d8ef1ec
tzihklearn <1922941549@qq.com>
2023-10-08 20:06:39 +0800

    feat(tcl):新增修改垃圾桶信息接口

--- tcl-server/src/main/java/org/sipc/tclserver/controller/GarbageController.java
+++ tcl-server/src/main/java/org/sipc/tclserver/controller/GarbageController.java
@@ -1,7 +1,9 @@
 package org.sipc.tclserver.controller;
 
 import  lombok.RequiredArgsConstructor;
+import org.sipc.tclserver.exection.DateBaseException;
 import org.sipc.tclserver.pojo.dto.CommonResult;
+import org.sipc.tclserver.pojo.dto.param.EditTrashParam;
 import org.sipc.tclserver.pojo.dto.param.GarbageAllParam;
 import org.sipc.tclserver.pojo.dto.result.DataResult;
 import org.sipc.tclserver.pojo.dto.result.GarbageAllResult;
@@ -29,6 +31,15 @@ public class GarbageController {
     @PostMapping("/add")
     public CommonResult<String> add(@RequestBody GarbageAllParam garbageAllParam) {
         return garbageService.add(garbageAllParam);
+    }
+
+    @PostMapping("/edit")
+    public CommonResult<String> edit(@RequestBody EditTrashParam editTrashParam) {
+        try {
+            return garbageService.edit(editTrashParam);
+        } catch (DateBaseException e) {
+            return CommonResult.fail("请求异常，请稍后再试");
+        }
     }
 
     @GetMapping("/data")
