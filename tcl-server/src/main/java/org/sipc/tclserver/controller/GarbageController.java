f399f715d3ceb191d9c5b6250b696ec8bea837ec
tzihklearn <1922941549@qq.com>
2023-10-08 15:34:06 +0800

    feat(tcl):实现tcl的垃圾桶数据面板接口

--- tcl-server/src/main/java/org/sipc/tclserver/controller/GarbageController.java
+++ tcl-server/src/main/java/org/sipc/tclserver/controller/GarbageController.java
@@ -1,8 +1,9 @@
 package org.sipc.tclserver.controller;
 
-import lombok.RequiredArgsConstructor;
+import  lombok.RequiredArgsConstructor;
 import org.sipc.tclserver.pojo.dto.CommonResult;
 import org.sipc.tclserver.pojo.dto.param.GarbageAllParam;
+import org.sipc.tclserver.pojo.dto.result.DataResult;
 import org.sipc.tclserver.pojo.dto.result.GarbageAllResult;
 import org.sipc.tclserver.service.GarbageService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,5 +29,10 @@ public class GarbageController {
     @PostMapping("/add")
     public CommonResult<String> add(@RequestBody GarbageAllParam garbageAllParam) {
         return garbageService.add(garbageAllParam);
+    }
+
+    @GetMapping("/data")
+    public CommonResult<DataResult> data(@RequestParam Integer districtId) {
+        return null;
     }
 }
---
+++ tcl-server/src/main/java/org/sipc/tclserver/pojo/dto/result/po/StatusPo.java
@@ -0,0 +1,17 @@
+package org.sipc.tclserver.pojo.dto.result.po;
+
+import lombok.Data;
+
+/**
+ * @author tzih
+ * @version v1.0
+ * @since 2023.10.06
+ */
+@Data
+public class StatusPo {
+
+    private Integer normal;
+
+    private Integer fault;
+
+}
