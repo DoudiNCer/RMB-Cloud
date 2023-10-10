13c232522fb511978308793896c05269044a232c
DoudiNCer <doudi@outlook.lv>
2023-10-10 11:27:40 +0800

    少了几个文件

---
+++ control-server/src/main/java/org/sipc/controlserver/controller/user/ShopController.java
@@ -0,0 +1,27 @@
+package org.sipc.controlserver.controller.user;
+
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.sipc.controlserver.pojo.dto.CommonResult;
+import org.sipc.controlserver.pojo.dto.user.param.shop.ConventGiftParam;
+import org.sipc.controlserver.pojo.dto.user.result.shop.GetGiftsResult;
+import org.sipc.controlserver.service.user.ShopService;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/shop")
+//@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+public class ShopController {
+    @DubboReference
+    private ShopService shopService;
+
+    @GetMapping("/gifts")
+    public CommonResult<GetGiftsResult> getGifts(){
+        return shopService.getGifts();
+    }
+
+    @PostMapping("/convent")
+    public CommonResult<String> conventGift(@Validated @RequestParam ConventGiftParam param){
+        return shopService.conventGift(param);
+    }
+}
