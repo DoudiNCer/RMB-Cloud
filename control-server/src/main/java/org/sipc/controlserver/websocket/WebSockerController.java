023697dabb44616407d38dfe24743e5a73942be4
DoudiNCer <doudi@outlook.lv>
2023-10-31 22:12:56 +0800

    fix(*): 不小心把飞书机器人砍了

--- control-server/src/main/java/org/sipc/controlserver/websocket/WebSockerController.java
+++ control-server/src/main/java/org/sipc/controlserver/websocket/WebSockerController.java
@@ -1,4 +1,4 @@
-package org.sipc.controlserver.controller.tcl;
+package org.sipc.controlserver.websocket;
 
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArraySet;
